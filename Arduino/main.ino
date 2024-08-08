2709e8ece3c6e5b9586e619bb8227def58266f92
orion787 <116973576+orion787@users.noreply.github.com>
2024-08-08 11:29:33 +0300

    Update main.ino

--- Arduino/main.ino
+++ Arduino/main.ino
@@ -4,45 +4,41 @@
 
 //------------Config devices --------------------
 #define DHTTYPE DHT11   // DHT 11
-//DHT11Sensor dht {5, DHTTYPE};
 
-DHT11Sensor temp_sensors[5] = {DHT11Sensor(5, DHT11),DHT11Sensor(5, DHT11), DHT11Sensor(5, DHT11), DHT11Sensor(5, DHT11), DHT11Sensor(5, DHT11)};
-Relay relays[5] = {Relay{7, 0}, Relay{8, 0}, Relay{7, 0}, Relay{7, 0}, Relay{7, 0}};
+DHT11Sensor temp_sensors[4] = {DHT11Sensor(6, DHT11),DHT11Sensor(5, DHT11), DHT11Sensor(4, DHT11), DHT11Sensor(3, DHT11)};
+Relay relays[4] = {Relay{7, 0}, Relay{8, 0}, Relay{9, 0}, Relay{10, 0}};
 
-
-//I have only 1 instead of 5 
-DHT11Sensor& dht = temp_sensors[0];
 
 #define _LCD_TYPE 1 
 #include <LCD_1602_RUS.h>
-LCD_1602_RUS lcd(0x27, 16, 2);
+LCD_1602_RUS lcd(0x27, 16, 4);
 
 
 
-SensorData sensor_data[5]{SensorData{0.0f, 0.0f}};
+SensorData sensor_data[4] = {SensorData{0.0f, 0.0f}, SensorData{0.0f, 0.0f}, SensorData{0.0f, 0.0f}, SensorData{0.0f, 0.0f}};
 Event<SensorData> events[5] = {
   Event<SensorData>(
-    [](const SensorData& data) { return data.temperature > 25.0; },
+    [](const SensorData& data) { return data.temperature >= 25.0f; },
     []() { OnRelay(relays[0]); },
     []() { OffRelay(relays[0]); }
   ),
   Event<SensorData>(
-    [](const SensorData& data) { return data.temperature <20.0; },
+    [](const SensorData& data) { return data.temperature <= 20.0f; },
     []() { OnRelay(relays[1]); },
     []() { OffRelay(relays[1]); }
   ),
   Event<SensorData>(
-    [](const SensorData& data) { return data.humidity > 60.0; },
+    [](const SensorData& data) { return data.temperature >= 25.0f; },
     []() { OnRelay(relays[2]); },
     []() { OffRelay(relays[2]); }
   ),
   Event<SensorData>(
-    [](const SensorData& data) { return data.humidity < 30.0; },
+    [](const SensorData& data) { return data.temperature <= 20.0f; },
     []() { OnRelay(relays[3]); },
     []() { OffRelay(relays[3]); }
   ),
   Event<SensorData>(
-    [](const SensorData& data) { return data.temperature > 30.0; },
+    [](const SensorData& data) { return data.temperature >= 25.0f; },
     []() { OnRelay(relays[4]); },
     []() { OffRelay(relays[4]); }
   )
@@ -50,38 +46,38 @@ Event<SensorData> events[5] = {
 
 
 void setup() {
-  pinMode(3, OUTPUT);
+  for(int i = 0; i < 4; ++i){
+    temp_sensors[i].begin();
+  }
   pinMode(7, OUTPUT);
   pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
 
   lcd.begin();
   lcd.backlight();
   lcd.setCursor(0, 0);
-  lcd.print("Z");
 }
 
 
 void loop() {
-
-  for(int i = 0; i < 5; ++i){
-    dht.readData();
+  for(int i = 0; i < 4; ++i){
+    temp_sensors[i].readData();
     delay(50);
-    sensor_data[i].temperature = dht.getTempC();
-    sensor_data[i].humidity = dht.getTempHum();
+    sensor_data[i].temperature = temp_sensors[i].getTempC();
+    sensor_data[i].humidity = temp_sensors[i].getTempHum();
     delay(50);
     events[i].handleEvent(sensor_data[i]);
   }
 
-
-  lcd.setCursor(0, 0);
-  lcd.print("S1");
-  //lcd.print(i + 1);
-  lcd.print(": Temp ");
-  lcd.print(sensor_data[0].temperature);
-  lcd.print("C");
-  lcd.setCursor(0, 1);
-  lcd.print("Hum ");
-  lcd.print(sensor_data[0].humidity);
-  lcd.print("% ");
-  delay(1000);
+  //logging to lcd console
+  for(int i = 0; i < 4; ++i){
+    lcd.setCursor(0, i);
+    lcd.print("S");
+    lcd.print(i + 1);
+    lcd.print(": ");
+    lcd.print(sensor_data[i].temperature);
+    lcd.print("C");
+  }
+  delay(2000);
 }
